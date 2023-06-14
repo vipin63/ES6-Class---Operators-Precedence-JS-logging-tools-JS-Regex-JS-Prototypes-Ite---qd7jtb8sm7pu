557a805b7cd1fc52b7bf3361560d7ff88f6fc841
vipin63 <132770876+vipin63@users.noreply.github.com>
2023-06-14 16:10:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 //Write your code here
 
-
+class API{
+    #secure;  
+    constructor(URL, method="GET"){
+        this.url=URL;
+        this.method=method;
+        this.#secure=this.url.startsWith('https');
+    }
+    isSecure(){
+        return this.#secure;
+    }
+    updateURL(newurl){
+        this.url=newurl;
+        this.#secure=this.url.startsWith('https');
+    }
+}
 
 
 
